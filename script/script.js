d53b92b800aedef9b87c52693d91ce55cb61beeb
Joe Lott <rjplott@gmail.com>
2021-02-17 20:03:16 -0500

    feat(.popup): implement smooth popup transition

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// Variable declaration
+
 const userProfile = document.querySelector(".profile");
 const addImagePopup = document.querySelector(".popup_purpose_add-picture");
 const editUserPopup = document.querySelector(".popup_purpose_edit-user");
@@ -11,6 +13,8 @@ const userTitle = userProfile.querySelector(".profile__user-title");
 const editButton = userProfile.querySelector(".profile__edit-button");
 const addButton = userProfile.querySelector(".profile__add-image-button");
 const imageContainer = imagePopup.querySelector(".popup__full-image-container");
+const fullImage = imageContainer.querySelector(".popup__full-image");
+const imageCaption = imageContainer.querySelector(".popup__image-caption");
 
 const userNameInput = editUserPopup.querySelector(
   ".popup__text-input_type_name"
@@ -26,6 +30,8 @@ const imageLinkInput = addImagePopup.querySelector(
   ".popup__text-input_type_image-link"
 );
 
+// Utility Functions
+
 const createCard = (name, link) => {
   const newCard = cardTemplate.content.querySelector(".card").cloneNode(true);
   const newCardImage = newCard.querySelector(".card__image");
@@ -39,7 +45,8 @@ const createCard = (name, link) => {
   newCard
     .querySelector(".card__delete-button")
     .addEventListener("click", handleDeleteCard);
-  gallery.append(newCard);
+
+  return newCard;
 };
 
 const createInitialCards = () => {
@@ -70,27 +77,21 @@ const createInitialCards = () => {
     },
   ];
 
-  initialCards.forEach((card) => createCard(card.name, card.link));
+  initialCards.forEach((card) =>
+    gallery.append(createCard(card.name, card.link))
+  );
 };
 
 const openPopup = (popup) => {
-  setUserText();
   popup.classList.add("popup_opened");
 };
 
 const closePopup = (evt) => {
-  const currentPopup = evt.target.closest(".popup");
-
-  if (currentPopup.classList.contains("popup_purpose_view-full-picture")) {
-    currentPopup.querySelector(".popup__full-image").remove();
-    currentPopup.querySelector(".popup__image-caption").remove();
-  }
-
-  currentPopup.classList.remove("popup_opened");
+  evt.target.closest(".popup").classList.remove("popup_opened");
 };
 
 const createNewCard = () => {
-  createCard(imageTitleInput.value, imageLinkInput.value);
+  gallery.prepend(createCard(imageTitleInput.value, imageLinkInput.value));
   imageLinkInput.value = "";
   imageTitleInput.value = "";
 };
@@ -105,6 +106,29 @@ const updateUserProfile = () => {
   userTitle.textContent = userTitleInput.value;
 };
 
+// Event Handlers
+
+const handleOpenEditPopup = () => {
+  setUserText();
+  openPopup(editUserPopup);
+};
+
+const handleAddImagePopup = () => {
+  openPopup(addImagePopup);
+};
+
+const handleEditUserSubmit = (evt) => {
+  evt.preventDefault();
+  updateUserProfile();
+  closePopup(evt);
+};
+
+const handleAddImageSubmit = (evt) => {
+  evt.preventDefault();
+  createNewCard();
+  closePopup(evt);
+};
+
 const handleLikeButtonClick = (evt) => {
   evt.target.classList.toggle("card__like-button_liked");
 };
@@ -114,43 +138,25 @@ const handleDeleteCard = (evt) => {
 };
 
 const handleOpenImagePopup = (evt) => {
-  const fullImage = document.createElement("img");
-  const fullImageCaption = document.createElement("figcaption");
-
   fullImage.setAttribute("src", evt.target.style.backgroundImage.slice(5, -2));
-  fullImage.classList.add("popup__full-image");
-
-  fullImageCaption.textContent =
+  imageCaption.textContent =
     evt.target.nextElementSibling.firstElementChild.textContent;
-  fullImageCaption.classList.add("popup__image-caption");
-
-  imageContainer.append(fullImage, fullImageCaption);
 
   openPopup(imagePopup);
 };
 
+const addCloseButtonListeners = () => {
+  Array.from(closePopupButtons).forEach((button) =>
+    button.addEventListener("click", (evt) => closePopup(evt))
+  );
+};
+
+// Function Invocations
+
 createInitialCards();
+addCloseButtonListeners();
 
-editButton.addEventListener("click", () => {
-  openPopup(editUserPopup);
-});
-
-addButton.addEventListener("click", () => {
-  openPopup(addImagePopup);
-});
-
-Array.from(closePopupButtons).forEach((button) =>
-  button.addEventListener("click", (evt) => closePopup(evt))
-);
-
-editUserPopup.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  updateUserProfile();
-  closePopup(evt);
-});
-
-addImagePopup.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  createNewCard();
-  closePopup(evt);
-});
+editButton.addEventListener("click", handleOpenEditPopup);
+addButton.addEventListener("click", handleAddImagePopup);
+editUserPopup.addEventListener("submit", handleEditUserSubmit);
+addImagePopup.addEventListener("submit", handleAddImageSubmit);
