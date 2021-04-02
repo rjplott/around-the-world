5bcb32091c8068fb65c4b91390a463c51fa1522f
Joe Lott <rjplott@gmail.com>
2021-04-02 17:50:53 -0400

    complete webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,9 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            options: { importLoaders: 1 },
           },
+          "postcss-loader",
         ],
       },
       {
